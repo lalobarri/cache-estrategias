2917d0a85e7f63af9c03ca14f4059a111f739ed5
Eduardo Barrientos <eduardoBarri@gmail.com>
2022-07-01 17:46:26 -0500

    Tercera estrategia: cache dinámico

--- sw.js
+++ sw.js
@@ -1,55 +1,55 @@
-const CACHE_NAME = 'cache-1'
+//Para el problema de que se puede mezclar los elementos del 
+//cache que son esenciales, con los elementos no necesario y antiguos o 
+//desactualizados, se utilizará una contantante diferente para 
+//cada caso
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dinamic-v1';
+const CACHE_INMUTABLE_NAME='inmutable-v1';
 
-//App Shell en cache storage
-//Todo lo que la aplicación requiere para trabajar
-//Varia dependiendo de la aplicación
 self.addEventListener('install', e =>{
     //Cuando se instala se guardar las cosas necesarias
-    const cacheProm = caches.open(CACHE_NAME)
-        .then( cache =>{//Se regresa una promesa
-            return cache.addAll([
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
+        .then( caches =>{//Se regresa una promesa
+            return caches.addAll([
             '/',
             '/index.html',
             '/css/style.css',
             '/img/main.jpg',
-            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
             '/js/app.js'
             ]);
         });   
-    e.waitUntil( cacheProm ); 
+   
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME)
+        .then( cache => {
+            return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+        });
+
+    e.waitUntil( Promise.all([cacheProm, cacheInmutable]));
+
+    
 
 });
 
 
-//Para la estrategía de Cache Only
 self.addEventListener('fetch', e =>{
-    
-    
-    //1-Cache Only, cuando toda petición surge del Cache
-    //  Todo lo que sea que se esta pidiendo surge el Cache Storage
-
-    //Se va a todos los caches para ver si existe
-    //e.respondWith(caches.match( e.request)); //CACHE ONLY 
-    
-    //2- Cache with Network Fallback - Primero Cache y luego internet
     const respuesta = caches.match(e.request)
         .then(res => {
             if( res ) return res;
             //No existe el archivo
             //tengo que ir a la web
+            //Se toma en consideración que el cache dinámico puede crecer muy grande 
             console.log('No existe', e.request.url);
             return fetch( e.request).then(newResp => {
-                caches.open( CACHE_NAME)
+                caches.open( CACHE_DYNAMIC_NAME)
                 .then( cache => {
-                    cache.put( e.request, newResp);
-                   
-                    
+                    cache.put( e.request, newResp);    
                 });
                 return newResp.clone;
             });
         }); //Termina definición de constante respuesta
     e.respondWith( respuesta);
 });
+
 
 
 function limpiarCache( cacheName, numeroItems ) {
